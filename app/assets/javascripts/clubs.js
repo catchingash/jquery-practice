f4b5b34dadca5ea2531ff2323e9a392a6e7035ed
Ashley Watkins <AWatkins31@gmail.com>
2015-08-26 16:36:08 -0700

    WIP

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -1,7 +1,18 @@
 $(function() {
   $(".choose").click(function(event) {
-    // event.preventDefault();
-    // select the (grand)parent div of `this` with a class of club
-    // add a class of `chosen` to that div
+    event.preventDefault();
+    var button = $(this);
+    var clubDiv = button.parents('.club');
+
+    var formTag = button.parent('form');
+    var url = formTag.attr('action');
+    var type = formTag.attr('method');
+
+    $.ajax(url, {
+      type: type,
+      success: function (data) {
+        clubDiv.toggleClass('chosen');
+      }
+    });
   });
 });
